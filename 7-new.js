8378f64f539f52f89b33428cf3b12156dd248ad1
TORESTO <therealtoresto@gmail.com>
2020-04-28 16:12:09 +0300

    creating

---
+++ 7-new.js
@@ -0,0 +1,7 @@
+const sum = new Function('a, b', 'return a + b');
+
+console.dir({
+    name: sum.name,
+    length: sum.length,
+    toString: sum.toString()
+});
---
+++ c-comments.js
@@ -0,0 +1,20 @@
+`use strict`;
+
+const fs = require('fs');
+
+const printLine = (
+    // Print specified line from a file
+    fileName, //file to parse
+    lineNumber //number, starting from 1
+    //Returns: boolean, success status
+) => {
+    const content = fs.readFileSync(fileName).toString();
+    const lines = content.split('\n');
+    const line = lines[lineNumber - 1];
+    if (line === undefined) return false;
+    console.dir({
+        fileName, lineNumber, line
+    });
+};
+
+printLine('./c-comments.js', 5)
